bd7d6373184bebf1bb97420987268269518407c8
sangcheol12 <eric2057@daum.net>
2023-04-24 13:18:44 +0900

    make mornitoring
    make mornitoring using webview

--- app/src/main/java/com/opensource/sls/MenuActivity.java
+++ app/src/main/java/com/opensource/sls/MenuActivity.java
@@ -1,5 +1,7 @@
 package com.opensource.sls;
 
+import android.content.Intent;
+import android.media.MediaParser;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
@@ -7,6 +9,7 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
+import android.widget.Button;
 
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -16,10 +19,21 @@ import androidx.navigation.ui.NavigationUI;
 import com.opensource.sls.databinding.ActivityMenuBinding;
 
 public class MenuActivity extends AppCompatActivity {
+    private View monitoringButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        monitoringButton = findViewById(R.id.monitoringButton);
+
+        monitoringButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+                Intent intent = new Intent(getApplicationContext(), MonitoringActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
